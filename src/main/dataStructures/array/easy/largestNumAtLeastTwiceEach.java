9ee640f57fc0be292f7b76a67cec15f30a60de70
Gayatri <gayatri-tryambak.matole@dbtechhackathon.com>
2024-02-29 17:12:14 +0530

    Added first solution for largestNum where its greater than atleast twice of each element

---
+++ src/main/dataStructures/array/easy/largestNumAtLeastTwiceEach.java
@@ -0,0 +1,27 @@
+package dataStructures.array.easy;
+
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+public class largestNumAtLeastTwiceEach {
+    public static void main(String[] args) {
+        largestNumAtLeastTwiceEach s = new largestNumAtLeastTwiceEach();
+        int[] nums = new int[]{3,6,1,0};
+        int ind = s.dominantIndex(nums);
+        System.out.println("Largest Num Index: " + ind);
+    }
+
+    public int dominantIndex(int[] nums) {
+        int largestNum = Arrays.stream(nums).max().getAsInt();
+        int largeNumInd = -1 ;
+        for (int i=0; i < nums.length; i++)
+        {
+            if (nums[i] == largestNum)
+                largeNumInd = i;
+            else if(nums[i] * 2 > largestNum)
+                return -1;
+        }
+        return largeNumInd;
+
+    }
+}
