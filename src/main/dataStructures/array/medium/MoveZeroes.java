231861c3897106072d476a1cef2e3c849d1b1dd9
Gayatri <gayatri-tryambak.matole@dbtechhackathon.com>
2024-03-09 12:52:07 +0530

    Array - Move Zeroes from array to the end

---
+++ src/main/dataStructures/array/medium/MoveZeroes.java
@@ -0,0 +1,35 @@
+package dataStructures.array.medium;
+
+import java.util.Arrays;
+
+public class MoveZeroes {
+    public static void main(String[] args) {
+        MoveZeroes s = new MoveZeroes();
+
+        int[] nums = new int[]{0,1,0,3,12};
+
+        System.out.println("Given Array: ");
+        Arrays.stream(nums).forEach(n -> System.out.print(n + ","));
+        System.out.println("\n Expected Array: [1,3,12,0,0]");
+        s.moveZeroes(nums);
+        System.out.println("New Derived Array: ");
+        Arrays.stream(nums).forEach(n -> System.out.print(n + ","));
+
+    }
+
+    public void moveZeroes(int[] nums) {
+        int idx =0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0){
+                nums[idx] = nums[i];
+                idx++;
+            }
+        }
+
+        for (int i = idx; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+       return;
+
+    }
+}
